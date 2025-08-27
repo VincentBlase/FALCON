a74bd03c09a2f2f6e73a335784a2d6288855c5f9
VincentBlase <63648607+VincentBlase@users.noreply.github.com>
2025-08-27 10:49:13 +0200

    Create run_example.py

---
+++ FALCON/run_example.py
@@ -0,0 +1,22 @@
+import numpy as np
+from FALCON.FALCON import FALCON
+from mmd_critic.kernels import RBFKernel
+from FALCON.open_data import open_dataset
+from FALCON.compute_score import compute_score
+
+name='IRIS'
+X,y = open_dataset(name)
+print('DATASET : ', name)
+
+max_budget = 20
+sigma1 = 0.1
+sigma2 = 0.1
+
+ARI_FALCON = []
+NMI_FALCON = []
+silhouette_FALCON = []
+
+clusterer_FALCON = FALCON(X,y,RBFKernel(0.25), RBFKernel(0.25))
+clustering_FALCON, intermediate_clustering = clusterer_FALCON.cluster(n=max_budget,k_proto=2, train_indices = None)
+    
+ARI_FALCON, NMI_FALCON, silhouette_FALCON = compute_score(intermediate_clustering,max_budget)
